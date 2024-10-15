6a1a67673833a1205bb51e7cfc7b1bb1d34b6194
Dayoung <kimdayoung771@gmail.com>
2024-10-15 17:06:14 +0900

    doPost

--- mybatis/src/main/java/com/yedam/web/MemberAddServlet.java
+++ mybatis/src/main/java/com/yedam/web/MemberAddServlet.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.yedam.common.DataSource;
+import com.yedam.dao.MemberMapper;
+import com.yedam.vo.Member;
+
 @WebServlet("/MemberAddServlet")
 public class MemberAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -24,9 +30,35 @@ public class MemberAddServlet extends HttpServlet {
 		out.print("<h3>web browser 한글</h3>");
 		out.print("<h3>web browser 한글222222</h3>");
 		out.print("<a href='index.html'>첫페이지로 이동</a>");
+		
+		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("mid");
+		String name = request.getParameter("mname");
+		String password = request.getParameter("pass");
+		String phone = request.getParameter("phone");
+		
+		Member member = new Member();
+		member.setMemberId(id);
+		member.setMemberName(name);
+		member.setPassword(password);
+		member.setPhone(phone);
+		
+		// openSession에 true로 파라미터 넘겨주면 자동 커밋됨
+		SqlSession sqlSession = DataSource.getInstance().openSession(true);
+		MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
+		
+		try {
+			if(dao.insertMember(member) == 1) {
+				response.getWriter().print("OK");
+			}		
+		} catch (Exception e) {
+			response.getWriter().print("NG");
+		}
+	
 	}
 
 }
